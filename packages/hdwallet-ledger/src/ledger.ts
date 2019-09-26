ef0570c6a4338b4a0bcd4320ef26c42238db8630
millsmcilroy <37815163+millsmcilroy@users.noreply.github.com>
2019-09-26 11:08:00 -0600

    Add "Ledger" as the return value for ledger.getLabel()

--- packages/hdwallet-ledger/src/ledger.ts
+++ packages/hdwallet-ledger/src/ledger.ts
@@ -366,7 +366,7 @@ export class LedgerHDWallet implements HDWallet, BTCWallet, ETHWallet {
   }
 
   public async getLabel (): Promise<string> {
-    return
+    return 'Ledger'
   }
 
   public async isLocked (): Promise<boolean> {
