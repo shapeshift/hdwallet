5f96efb37c7a1ae90b3e6ce94073c42bd6e08f13
Sean Luther <seanluther@Seans-MacBook-Pro.local>
2020-06-16 11:57:44 -0600

    fixes

--- packages/hdwallet-native/src/ethereum.ts
+++ packages/hdwallet-native/src/ethereum.ts
@@ -1,5 +1,6 @@
 import * as core from "@shapeshiftoss/hdwallet-core";
 import { Wallet, utils } from 'ethers'
+import { ETHGetAddress } from "@shapeshiftoss/hdwallet-core";
 
 export function MixinNativeETHWalletInfo<TBase extends core.Constructor>(
   Base: TBase
@@ -75,9 +76,9 @@ export function MixinNativeETHWallet<TBase extends core.Constructor>(
 ) {
   return class MixinNativeETHWallet extends Base {
     _supportsETH = true;
-    wallet: any
-    async ethInitializeWallet(mnemonic: string) {
-      this.wallet = await Wallet.fromMnemonic(mnemonic)
+    wallet: Wallet
+    ethInitializeWallet(mnemonic: string): void {
+      this.wallet = Wallet.fromMnemonic(mnemonic)
     }
     async ethGetAddress(msg: core.ETHGetAddress): Promise<string> {
       return this.wallet.getAddress()
