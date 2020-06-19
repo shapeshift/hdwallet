5e4c3576de06ea630307b5c782b41cdd7df0a2bb
kaladinlight <kaladin.light@gmail.com>
2020-06-19 14:00:06 -0600

    Fix core type and isNative check

--- packages/hdwallet-native/src/bitcoin.ts
+++ packages/hdwallet-native/src/bitcoin.ts
@@ -14,6 +14,8 @@ const supportedCoins = [
 
 const segwit = ["p2wpkh", "p2sh-p2wpkh"];
 
+type BTCScriptType = core.BTCInputScriptType | core.BTCOutputScriptType;
+
 type NonWitnessUtxo = Buffer;
 
 type WitnessUtxo = {
@@ -138,7 +140,7 @@ export function MixinNativeBTCWallet<TBase extends core.Constructor>(
     getKeyPair(
       coin: core.Coin,
       addressNList: core.BIP32Path,
-      scriptType?: core.BTCScriptType
+      scriptType?: BTCScriptType
     ): bitcoin.ECPairInterface {
       const network = getNetwork(coin, scriptType);
       const wallet = bitcoin.bip32.fromSeed(this.seed, network);
@@ -148,7 +150,7 @@ export function MixinNativeBTCWallet<TBase extends core.Constructor>(
 
     createPayment(
       pubkey: Buffer,
-      scriptType: core.BTCScriptType,
+      scriptType: BTCScriptType,
       network?: bitcoin.Network
     ): bitcoin.Payment {
       switch (scriptType) {
