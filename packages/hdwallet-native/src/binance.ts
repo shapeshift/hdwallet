8e4ac52b567c4dbeb25498e547ab59a43ced0ca2
Highlander <BitHighlander@users.noreply.github.com>
2020-10-21 13:17:44 -0700

    Update packages/hdwallet-native/src/binance.ts
    Co-authored-by: Christopher <39497236+majorhayes@users.noreply.github.com>

--- packages/hdwallet-native/src/binance.ts
+++ packages/hdwallet-native/src/binance.ts
@@ -81,7 +81,7 @@ export function MixinNativeBinanceWallet<TBase extends core.Constructor<NativeHD
         await client.setPrivateKey(privateKey);
         await client.initChain();
 
-        const addressFrom = msg.tx.msgs[0].inputs[0].address;
+        const addressFrom = msg.tx?.msgs?.[0]?..inputs?.[0]?.address;
         const addressFromVerify = client.getClientKeyAddress()
         if (addressFrom !== addressFromVerify) {
           throw Error("Invalid permissions to sign for address")
