e91d9826fb7725846a61d6f28190946c82ebc89f
Reid Rankin <reidrankin@gmail.com>
2021-05-19 12:05:01 -0400

    stricter types in native pairDevice()

--- packages/hdwallet-native/src/adapter.ts
+++ packages/hdwallet-native/src/adapter.ts
@@ -1,5 +1,5 @@
 import * as core from "@shapeshiftoss/hdwallet-core";
-import { NativeHDWallet } from "./native";
+import { NativeHDWallet, isNative } from "./native";
 import * as Isolation from "./crypto/isolation";
 
 export type NativeAdapterArgs = {
@@ -27,11 +27,12 @@ export class NativeAdapter {
     if (!wallet && deviceId) {
       // If a wallet with that ID hasn't been added to the keychain, then create it
       wallet = new NativeHDWallet({ deviceId });
+      if (!isNative(wallet)) throw new Error("expected native wallet");
       this.keyring.add(wallet, deviceId);
       this.keyring.decorateEvents(deviceId, wallet.events);
     }
 
-    if (wallet?._isNative) {
+    if (isNative(wallet)) {
       const id = await wallet.getDeviceID();
       this.keyring.emit([wallet.getVendor(), id, core.Events.CONNECT], id);
 
