1b41a1fc4239fa105c76f524e61206a8d6f43613
highlander <bithighlander@gmail.com>
2021-06-05 16:57:21 -0600

    expand eth signtx example

--- integration/src/ethereum/ethereum.ts
+++ integration/src/ethereum/ethereum.ts
@@ -146,6 +146,36 @@ export function ethereumTests(get: () => { wallet: HDWallet; info: HDWalletInfo 
     );
 
     test(
+      "ethSignTx() - ETH (0 nonce)",
+      async () => {
+        if (!wallet) return;
+        let res = await wallet.ethSignTx({
+          addressNList: bip32ToAddressNList("m/44'/60'/0'/0/0"),
+          nonce: "0x0",
+          gasPrice: "0x1dcd65000",
+          gasLimit: "0x5622",
+          value: "0x2c68af0bb14000",
+          to: "0x12eC06288EDD7Ae2CC41A843fE089237fC7354F0",
+          chainId: 1,
+          data: "",
+        });
+        expect(res).toEqual({
+          r: "0x63db3dd3bf3e1fe7dde1969c0fc8850e34116d0b501c0483a0e08c0f77b8ce0a",
+          s: "0x28297d012cccf389f6332415e96ee3fc0bbf8474d05f646e029cd281a031464b",
+          v: 38,
+          serialized:
+            "0xf86b018501dcd650008256229412ec06288edd7ae2cc41a843fe089237fc7354f0872c68af0bb140008026a063db3dd3bf3e1fe7dde1969c0fc8850e34116d0b501c0483a0e08c0f77b8ce0aa028297d012cccf389f6332415e96ee3fc0bbf8474d05f646e029cd281a031464b",
+        });
+
+        //decode
+        const { from } = ethers.utils.parseTransaction(res.serialized)
+        expect(from).toEqual("0x3f2329C9ADFbcCd9A84f52c906E936A42dA18CB8")
+
+      },
+      TIMEOUT
+    );
+
+    test(
       "ethSignTx() - ERC20",
       async () => {
         if (!wallet) return;
