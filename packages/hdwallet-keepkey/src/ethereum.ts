0d7033214b9a3b2c8dd5dddc07ec40b1a98d4079
highlander <bithighlander@gmail.com>
2021-06-09 09:30:25 -0600

    type bs

--- packages/hdwallet-keepkey/src/ethereum.ts
+++ packages/hdwallet-keepkey/src/ethereum.ts
@@ -71,7 +71,8 @@ export async function ethSignTx(transport: KeepKeyTransport, msg: ETHSignTx): Pr
   return transport.lockDuring(async () => {
     const est: EthereumSignTx = new EthereumSignTx();
     est.setAddressNList(msg.addressNList);
-    const idx = arrayify(msg.nonce).findIndex((x) => x != 0x00);
+    const idx:(number | Uint8Array) = arrayify(msg.nonce).findIndex((x) => x != 0x00);
+    // @ts-ignore
     est.setNonce(idx > -1 ? msg.nonce?.slice(idx) : [0x00]);
     est.setGasPrice(arrayify(msg.gasPrice));
     est.setGasLimit(arrayify(msg.gasLimit));
