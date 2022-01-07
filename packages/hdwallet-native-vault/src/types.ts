cfd9a5c3fb5592b3431b22d31c577cf55edefd87
Reid Rankin <reidrankin@gmail.com>
2022-01-07 18:26:03 -0500

    vault: factor out ISealable type

--- packages/hdwallet-native-vault/src/types.ts
+++ packages/hdwallet-native-vault/src/types.ts
@@ -1,6 +1,8 @@
 import type { IArgon2Options } from "hash-wasm";
 import type * as idb from "idb-keyval";
 
+import type { Revocable } from "./util";
+
 export type AsyncCrypto = Omit<Crypto, "getRandomValues"> & {
   getRandomValues<T extends DataView | Float32Array | Float64Array | Uint8ClampedArray | Uint8Array | Int8Array | Int16Array | Int32Array | Uint16Array | Uint32Array | null>(array: T): T | Promise<T>;
 }
@@ -23,6 +25,11 @@ export interface IVaultFactory<U extends IVaultBackedBy<any> | IVault> {
   readonly delete: (id: string) => Promise<void>;
 }
 
+export interface ISealableVaultFactory<U extends ISealable & (IVaultBackedBy<any> | IVault)> extends IVaultFactory<U> {
+  readonly create: (password?: string, sealed?: boolean) => Promise<U>;
+  readonly open: (id?: string, password?: string, sealed?: boolean) => Promise<U>;
+}
+
 export interface IVaultBackedBy<T> {
   readonly id: string;
   readonly meta: Map<string, unknown>;
@@ -31,7 +38,13 @@ export interface IVaultBackedBy<T> {
   save(serialize: () => Promise<T>): Promise<this>;
 }
 
-export interface IVault extends Map<string, Promise<unknown>> {
+export interface ISealable extends Revocable {
+  readonly sealed: boolean;
+  seal(): void;
+  unwrap(addRevoker?: (revoke: () => void) => void): this;
+}
+
+export interface IVault extends Map<string, Promise<unknown>>, ISealable, Revocable {
   readonly id: string;
   readonly meta: Map<string, unknown>;
   setPassword(password: string): Promise<this>;
