8a81b4480938d2167879def48107e8c6e506b840
Reid Rankin <reidrankin@gmail.com>
2022-01-07 18:37:04 -0500

    vault: test logging changes

--- packages/hdwallet-native-vault/src/index.test.ts
+++ packages/hdwallet-native-vault/src/index.test.ts
@@ -77,16 +77,16 @@ describe("Vault", () => {
     expect((await Vault.list()).length).toBe(1);
     expect(vault.meta.get("name")).toBe("default");
 
-    console.log(await idb.entries(keyStore));
-    console.log(await idb.entries(vaultStore));
+    console.log("keyStore", await idb.entries(keyStore));
+    console.log("vaultStore", await idb.entries(vaultStore));
   });
 
   it("should open a vault", async () => {
     const vaultIDs = await Vault.list();
     expect(vaultIDs.length).toBe(1);
     const vault = await Vault.open(vaultIDs[0], "foobar");
-    console.log(jose.decodeProtectedHeader((await idb.get(vaultIDs[0], vaultStore))!));
-    console.log("entries", vault.entries());
+    // console.log(jose.decodeProtectedHeader((await idb.get(vaultIDs[0], vaultStore))!));
+    // console.warn("entries", Array.from(vault.entries()));
     expect(await vault.get("foo")).toBe("bar");
     expect(uuid.validate(vault.id)).toBe(true);
     expect(vault.meta.size).toBe(1);
