9267695735de25efba41d982177fc435baa0870d
Amin Limbada <aminlimbada94@gmail.com>
2022-01-18 14:31:24 +0400

    chore: remove describeETHPath method and add unit test for ethereum.ts

--- packages/hdwallet-xdefi/src/ethereum.ts
+++ packages/hdwallet-xdefi/src/ethereum.ts
@@ -1,36 +1,6 @@
 import * as core from "@shapeshiftoss/hdwallet-core";
 import { ETHSignedMessage } from "@shapeshiftoss/hdwallet-core";
 
-export function describeETHPath(path: core.BIP32Path): core.PathDescription {
-  let pathStr = core.addressNListToBIP32(path);
-  let unknown: core.PathDescription = {
-    verbose: pathStr,
-    coin: "Ethereum",
-    isKnown: false,
-  };
-
-  if (path.length !== 5) return unknown;
-
-  if (path[0] !== 0x80000000 + 44) return unknown;
-
-  if (path[1] !== 0x80000000 + core.slip44ByCoin("Ethereum")) return unknown;
-
-  if ((path[2] & 0x80000000) >>> 0 !== 0x80000000) return unknown;
-
-  if (path[3] !== 0) return unknown;
-
-  if (path[4] !== 0) return unknown;
-
-  let index = path[2] & 0x7fffffff;
-  return {
-    verbose: `Ethereum Account #${index}`,
-    accountIdx: index,
-    wholeAccount: true,
-    coin: "Ethereum",
-    isKnown: true,
-  };
-}
-
 export async function ethVerifyMessage(msg: core.ETHVerifyMessage, ethereum: any): Promise<boolean | null> {
   console.error("Method ethVerifyMessage unsupported for XDeFi wallet!");
   return null;
