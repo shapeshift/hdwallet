4a5f3768b9f0dc1db1885cf97cd5af3d84f31a73
Amin Limbada <aminlimbada94@gmail.com>
2022-02-16 17:26:51 +0400

    chore: cleanup bitocin.ts

--- packages/hdwallet-xdefi/src/bitcoin.ts
+++ packages/hdwallet-xdefi/src/bitcoin.ts
@@ -1,5 +1,6 @@
 import * as core from "@shapeshiftoss/hdwallet-core";
 import * as bitcoinMsg from "bitcoinjs-message";
+import Base64 from "base64-js";
 
 export async function btcGetAddress(bitcoin: any): Promise<string | null> {
   if (!bitcoin) throw new Error("XDEFI Bitcoin Provider not found");
@@ -26,44 +27,7 @@ export async function btcGetAddress(bitcoin: any): Promise<string | null> {
   });
 }
 
-/**
- * return new Promise((res, rej) => {
-    try {
-      bitcoin.request(
-        {
-          method: "transfer",
-          params: [
-            {
-              asset: {
-                chain: "BTC",
-                symbol: "BTC",
-                ticker: "BTC",
-              },
-              from: address,
-              amount: {
-                amount: inputs[0].amount,
-                decimals: 8,
-              },
-              recipient: outputs[0].address,
-            },
-          ],
-        },
-        (err: any, result: any) => {
-          console.log("🚀 ~ file: bitcoin.ts ~ line 44 ~ returnnewPromise ~ bitcoinAccounts", result);
-          if (err) rej(err);
-          res(result);
-        }
-      );
-    } catch (error) {
-      console.error(error);
-      rej(error);
-    }
-  }); 
-  
-  */
 export async function btcSignTx(msg: core.BTCSignTx, address: string, bitcoin: any): Promise<core.BTCSignedTx | null> {
-  const { coin, inputs, outputs, version, locktime } = msg;
-
   if (!bitcoin) throw new Error("XDEFI Bitcoin Provider not found");
 
   if (!(bitcoin && bitcoin.request)) {
@@ -74,27 +38,15 @@ export async function btcSignTx(msg: core.BTCSignTx, address: string, bitcoin: a
     try {
       bitcoin.request(
         {
-          method: "transfer",
-          params: [
-            {
-              msg,
-              from: address,
-              recipient: msg.outputs[0].address,
-              amount: {
-                amount: "0",
-                decimals: 8,
-              },
-            },
-          ],
+          method: "sign_transaction",
+          params: [{ msg, from: address }],
         },
-        (err: any, result: any) => {
-          console.log("🚀 ~ file: bitcoin.ts ~ line 44 ~ returnnewPromise ~ bitcoinAccounts", result);
+        (err: any, result: core.BTCSignedTx) => {
           if (err) rej(err);
           res(result);
         }
       );
     } catch (error) {
-      console.error(error);
       rej(error);
     }
   });
@@ -135,7 +87,8 @@ export async function btcSignMessage(
 
 export async function btcVerifyMessage(msg: core.BTCVerifyMessage) {
   try {
-    return bitcoinMsg.verify(msg.message, msg.address, msg.signature, undefined, true);
+    const signature = Base64.fromByteArray(core.fromHexString(msg.signature));
+    return bitcoinMsg.verify(msg.message, msg.address, signature);
   } catch (error) {
     console.log(`Error ocurred while verifying signature: ${error}`);
     return false;
