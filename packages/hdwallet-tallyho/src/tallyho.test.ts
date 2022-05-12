96bb7c5caf240ff7a764c7dfef22fbb48cd58e69
Pasta Ghost <pastaghost@2520.io>
2022-05-12 12:56:57 -0600

    fix build errors

--- packages/hdwallet-tallyho/src/tallyho.test.ts
+++ packages/hdwallet-tallyho/src/tallyho.test.ts
@@ -158,7 +158,6 @@ describe("TallyHoHDWallet", () => {
     const hash = await wallet.ethSendTx({
       addressNList: core.bip32ToAddressNList("m/44'/60'/0'/0/0"),
       nonce: "0xDEADBEEF",
-      gasPrice: "0xDEADBEEF",
       gasLimit: "0xDEADBEEF",
       maxFeePerGas: "0xDEADBEEF",
       to: "0xDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEF",
