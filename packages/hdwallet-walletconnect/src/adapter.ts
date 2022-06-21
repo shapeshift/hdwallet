d3c190a7887801da08cbf97e076873dc105087da
Pasta Ghost <pastaghost@2520.io>
2022-06-21 13:40:28 -0600

    refactor: set walletconnect provider config in adapter constructor

--- packages/hdwallet-walletconnect/src/adapter.ts
+++ packages/hdwallet-walletconnect/src/adapter.ts
@@ -3,23 +3,35 @@ import WalletConnectProvider from "@walletconnect/web3-provider";
 
 import { WalletConnectHDWallet } from "./walletconnect";
 
+export type WalletConnectProviderConfig =
+  | {
+      infuraId: string;
+    }
+  | { rpc: { [key: number]: string } };
+
 export class WalletConnectAdapter {
   keyring: core.Keyring;
+  private providerConfig: WalletConnectProviderConfig;
 
-  private constructor(keyring: core.Keyring) {
+  private constructor(keyring: core.Keyring, config: WalletConnectProviderConfig) {
     this.keyring = keyring;
+    this.providerConfig = config;
   }
 
-  public static useKeyring(keyring: core.Keyring) {
-    return new WalletConnectAdapter(keyring);
+  public static useKeyring(keyring: core.Keyring, config: WalletConnectProviderConfig) {
+    return new WalletConnectAdapter(keyring, config);
   }
 
   public async initialize(): Promise<number> {
     return Object.keys(this.keyring.wallets).length;
   }
 
-  public async pairDevice(provider: WalletConnectProvider): Promise<WalletConnectHDWallet> {
+  public async pairDevice(): Promise<WalletConnectHDWallet> {
     try {
+      if (!this.providerConfig) {
+        throw new Error("WalletConnect provider configuration not set.");
+      }
+      const provider = new WalletConnectProvider(this.providerConfig);
       const wallet = new WalletConnectHDWallet(provider);
 
       //  Enable session (triggers QR Code modal)
