f8801652e444f901ab95765028c8abba87c3424c
Pasta Ghost <pastaghost@2520.io>
2022-11-15 21:18:12 -0700

    fix: remove entire exchangeType checks

--- packages/hdwallet-trezor/src/ethereum.ts
+++ packages/hdwallet-trezor/src/ethereum.ts
@@ -41,8 +41,6 @@ export async function ethSignTx(
   if (msg.toAddressNList !== undefined && !(await ethSupportsSecureTransfer()))
     throw new Error("Trezor does not support SecureTransfer");
 
-  if (!ethSupportsNativeShapeShift()) throw new Error("Trezor does not support Native ShapeShift");
-
   const utx = {
     to: msg.to,
     value: msg.value,
