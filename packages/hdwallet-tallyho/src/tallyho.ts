63e0fc9941244a20fa867b018016c7cc408bc2de
Łukasz Myśliński <lukasz@mlnative.com>
2023-04-12 13:00:22 +0200

    Fix talyho wallet

--- packages/hdwallet-tallyho/src/tallyho.ts
+++ packages/hdwallet-tallyho/src/tallyho.ts
@@ -90,7 +90,7 @@ export class TallyHoHDWallet implements core.HDWallet, core.ETHWallet {
   readonly _supportsEthSwitchChain = false;
   readonly _supportsAvalanche = false;
   readonly _supportsOptimism = false;
-  readonly _supportsPolygon = false;
+  readonly _supportsPolygon = true;
   readonly _supportsBSC = false;
   readonly _isTallyHo = true;
 
