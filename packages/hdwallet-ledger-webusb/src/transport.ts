9c365932055752594fb8d909cc98d46d79d575e2
gomes <17035424+gomesalexandre@users.noreply.github.com>
2023-09-25 20:40:02 +0200

    feat: getTransport on call()

--- packages/hdwallet-ledger-webusb/src/transport.ts
+++ packages/hdwallet-ledger-webusb/src/transport.ts
@@ -127,6 +127,8 @@ export class LedgerWebUsbTransport extends ledger.LedgerTransport {
     );
 
     try {
+      // Re-creates transport on method call, which will reconnect the device if disconnected
+      await getTransport();
       const methodInstance: LedgerTransportMethod<T, U> = translateCoinAndMethod(this.transport, coin, method);
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore ts is drunk, stop pls
