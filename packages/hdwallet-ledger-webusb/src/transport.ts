f0bfcadb38e1704805d8bf0fc6405968ef209c0d
gomes <17035424+gomesalexandre@users.noreply.github.com>
2023-09-25 12:12:37 +0200

    feat: open, reset and claim interface on getTransport()

--- packages/hdwallet-ledger-webusb/src/transport.ts
+++ packages/hdwallet-ledger-webusb/src/transport.ts
@@ -53,6 +53,8 @@ export async function getTransport(): Promise<TransportWebUSB> {
 
   try {
     const device = await getFirstLedgerDevice();
+    await device.open();
+    await device.reset();
 
     const maybeDeviceInterface = device.configurations[0].interfaces.find(({ alternates }: any) =>
       alternates.some(({ interfaceClass }: any) => interfaceClass === 255)
@@ -60,6 +62,7 @@ export async function getTransport(): Promise<TransportWebUSB> {
 
     if (!maybeDeviceInterface) throw new Error("No Ledger device found");
     const { interfaceNumber } = maybeDeviceInterface;
+    await device.claimInterface(interfaceNumber);
 
     const transport = new TransportWebUSB(device, interfaceNumber);
     return transport;
