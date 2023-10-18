feafe29d2ba8ac61b6d684078fe43fad431426c7
Apotheosis <97164662+0xApotheosis@users.noreply.github.com>
2023-10-18 11:10:18 +1100

    chore: improve types

--- packages/hdwallet-native/src/crypto/isolation/engines/default/bip39.ts
+++ packages/hdwallet-native/src/crypto/isolation/engines/default/bip39.ts
@@ -1,19 +1,16 @@
 /// <reference types="bip32/types/crypto" />
 
-import * as core from "@shapeshiftoss/hdwallet-core";
-import * as bip32crypto from "bip32/src/crypto";
 import { createSHA512, pbkdf2 } from "hash-wasm";
 import { TextEncoder } from "web-encoding";
 
-import * as BIP32 from "../../core/bip32";
-import * as BIP39 from "../../core/bip39";
-import { safeBufferFrom } from "../../types";
-import * as BIP32Engine from "./bip32";
+import type { Seed as SeedType } from "../../core/bip32";
+import type { Mnemonic as Bip39Mnemonic } from "../../core/bip39";
+import { Seed } from "./bip32";
 import { Revocable, revocable } from "./revocable";
 
 export * from "../../core/bip39";
 
-export class Mnemonic extends Revocable(class {}) implements BIP39.Mnemonic {
+export class Mnemonic extends Revocable(class {}) implements Bip39Mnemonic {
   readonly #mnemonic: string;
 
   protected constructor(mnemonic: string) {
@@ -21,18 +18,18 @@ export class Mnemonic extends Revocable(class {}) implements BIP39.Mnemonic {
     this.#mnemonic = mnemonic.normalize("NFKD");
   }
 
-  static async create(mnemonic: string): Promise<BIP39.Mnemonic> {
+  static async create(mnemonic: string): Promise<Bip39Mnemonic> {
     const obj = new Mnemonic(mnemonic);
     return revocable(obj, (x) => obj.addRevoker(x));
   }
 
-  async toSeed(passphrase?: string): Promise<BIP32.Seed> {
+  async toSeed(passphrase?: string): Promise<SeedType> {
     if (passphrase !== undefined && typeof passphrase !== "string") throw new Error("bad passphrase type");
 
     const mnemonic = this.#mnemonic;
     const salt = new TextEncoder().encode(`mnemonic${passphrase ?? ""}`.normalize("NFKD"));
 
-    const out = await BIP32Engine.Seed.create(
+    const out = await Seed.create(
       Buffer.from(
         await pbkdf2({
           password: mnemonic,
