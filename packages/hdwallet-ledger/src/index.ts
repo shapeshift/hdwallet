4974a65d04c352407401ff616ec238ffd04360cc
pastaghost <pastaghost@2520.io>
2024-01-30 13:46:51 -0700

    feat(common): remove unused wallet adapters

--- packages/hdwallet-ledger/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./bitcoin";
-export * from "./ethereum";
-export * from "./thorchain";
-export * from "./ledger";
-export * from "./transport";
-export * from "./utils";
--- packages/hdwallet-metamask-shapeshift-multichain/src/common.ts
+++
@@ -1,2 +0,0 @@
-export const SNAP_ID = "npm:@shapeshiftoss/metamask-snaps";
-// export const SNAP_ID = "local:http://localhost:9000";
