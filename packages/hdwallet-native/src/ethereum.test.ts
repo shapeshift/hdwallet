2e71c994aca9cf7eda726151dabcd7764654b676
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-02-22 16:09:16 -0700

    feat: parseTransaction -> Transaction.from

--- packages/hdwallet-native/src/ethereum.test.ts
+++ packages/hdwallet-native/src/ethereum.test.ts
@@ -1,5 +1,5 @@
 import * as core from "@shapeshiftoss/hdwallet-core";
-import { parseTransaction } from "ethers";
+import { Transaction } from "ethers";
 
 import * as native from "./native";
 
@@ -152,7 +152,7 @@ describe("NativeETHWallet", () => {
         "v": 38,
       }
     `);*/
-    expect(parseTransaction(sig!.serialized).from).toEqual("0x73d0385F4d8E00C5e6504C6030F47BF6212736A8");
+    expect(Transaction.from(sig!.serialized).from).toEqual("0x73d0385F4d8E00C5e6504C6030F47BF6212736A8");
   });
 
   it("should sign a EIP-1559 transaction correctly", async () => {
@@ -186,7 +186,7 @@ describe("NativeETHWallet", () => {
         "v": 38,
       }
     `);*/
-    expect(parseTransaction(sig!.serialized).from).toEqual("0x73d0385F4d8E00C5e6504C6030F47BF6212736A8");
+    expect(Transaction.from(sig!.serialized).from).toEqual("0x73d0385F4d8E00C5e6504C6030F47BF6212736A8");
   });
 
   describe("sign and verify message", () => {
