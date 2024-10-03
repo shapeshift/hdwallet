d48968dc6b993b2405f3813f6aa9f27b7c84351b
gomes <17035424+gomesalexandre@users.noreply.github.com>
2024-10-03 12:36:44 +0200

    feat: more cleanup

--- packages/hdwallet-metamask-shapeshift-multichain/src/adapter.ts
+++ packages/hdwallet-metamask-shapeshift-multichain/src/adapter.ts
@@ -5,72 +5,6 @@ import { createStore } from "mipd";
 import { SNAP_ID } from "./common";
 import { MetaMaskShapeShiftMultiChainHDWallet } from "./shapeshift-multichain";
 
-// https://github.com/wevm/wagmi/blob/21245be51d7c6dff1c7b285226d0c89c4a9d8cac/packages/connectors/src/utils/getInjectedName.ts#L6-L56
-// This will need to be kept up-to-date with the latest list of impersonators
-// TODO(gomes): we shouldn't really need this anymore. We can now use rdns / name :tada:, *and* it will actually work
-const METAMASK_IMPERSONATORS = [
-  "isBraveWallet",
-  "isTokenary",
-  "isFrame",
-  "isLiquality",
-  "isOpera",
-  "isTally",
-  "isStatus",
-  "isXDEFI",
-  "isNifty",
-  "isRonin",
-  "isBinance",
-  "isCoinbase",
-  "isExodus",
-  "isPhantom",
-  "isGlow",
-  "isOneInch",
-  "isRabby",
-  "isTrezor",
-  "isLedger",
-  "isKeystone",
-  "isBitBox",
-  "isGridPlus",
-  "isJade",
-  "isPortis",
-  "isFortmatic",
-  "isTorus",
-  "isAuthereum",
-  "isWalletLink",
-  "isWalletConnect",
-  "isDapper",
-  "isBitski",
-  "isVenly",
-  "isSequence",
-  "isGamestop",
-  "isZerion",
-  "isDeBank",
-  "isKukai",
-  "isTemple",
-  "isSpire",
-  "isWallet",
-  "isCore",
-  "isAnchor",
-  "isWombat",
-  "isMathWallet",
-  "isMeetone",
-  "isHyperPay",
-  "isTokenPocket",
-  "isBitpie",
-  "isAToken",
-  "isOwnbit",
-  "isHbWallet",
-  "isMYKEY",
-  "isHuobiWallet",
-  "isEidoo",
-  "isTrust",
-  "isImToken",
-  "isONTO",
-  "isSafePal",
-  "isCoin98",
-  "isVision",
-];
-
 const store = createStore();
 
 export class MetaMaskAdapter {
@@ -95,9 +29,7 @@ export class MetaMaskAdapter {
     if (!maybeEip6963Provider) throw new Error("EIP-6963 provider not found");
     const eip1193Provider = maybeEip6963Provider.provider;
 
-    // Checking for the truthiness of the value isn't enough - some impersonators have the key present but undefined
-    // This is weird, but welcome to the world of web3
-    const isMetaMaskImpersonator = METAMASK_IMPERSONATORS.some((impersonator) => impersonator in eip1193Provider);
+    const isMetaMaskImpersonator = maybeEip6963Provider.info.rdns !== "io.metamask";
 
     if (!isMetaMaskImpersonator && !shapeShiftSnapInstalled(SNAP_ID)) {
       console.info("ShapeShift Multichain snap not found. Prompting user to install.");
