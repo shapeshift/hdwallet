74db0fb6d4cc06fad54241582f6465c761814ab3
kaladinlight <35275952+kaladinlight@users.noreply.github.com>
2025-05-20 14:13:01 -0600

    feat: mayachain support for ledger

--- packages/hdwallet-ledger/src/index.ts
+++ packages/hdwallet-ledger/src/index.ts
@@ -1,6 +1,7 @@
 export * from "./bitcoin";
 export * from "./ethereum";
 export * from "./thorchain";
+export * from "./mayachain";
 export * from "./ledger";
 export * from "./transport";
 export * from "./utils";
