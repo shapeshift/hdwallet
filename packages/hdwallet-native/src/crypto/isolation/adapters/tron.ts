4119922572997efd0b7a801b4694f16f4e1c31d3
NeOMakinG <14963751+NeOMakinG@users.noreply.github.com>
2025-11-27 15:32:45 +0100

    fix: lint

--- packages/hdwallet-native/src/crypto/isolation/adapters/tron.ts
+++ packages/hdwallet-native/src/crypto/isolation/adapters/tron.ts
@@ -1,5 +1,5 @@
-import * as core from "@shapeshiftoss/hdwallet-core";
 import { keccak256 } from "@ethersproject/keccak256";
+import * as core from "@shapeshiftoss/hdwallet-core";
 import * as bs58check from "bs58check";
 
 import { Isolation } from "../..";
