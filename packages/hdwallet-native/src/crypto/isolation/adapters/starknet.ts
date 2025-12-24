746cf06a888caaf78385fca85d80ba97e550d39a
gomes <17035424+gomesalexandre@users.noreply.github.com>
2025-12-24 18:30:48 +0100

    docs: add comprehensive Stark curve documentation and references

--- packages/hdwallet-native/src/crypto/isolation/adapters/starknet.ts
+++ packages/hdwallet-native/src/crypto/isolation/adapters/starknet.ts
@@ -9,11 +9,33 @@ import { Isolation } from "../..";
  *
  * Uses dedicated Stark engine with STARK curve operations instead of
  * polluting the generic BIP32 secp256k1 engine with chain-specific code.
+ *
+ * ACCOUNT ABSTRACTION:
+ * All Starknet accounts are smart contracts (no EOAs). The account address is computed from:
+ * - Account contract class hash (OpenZeppelin, Argent, Braavos implementations)
+ * - Constructor calldata (including public key)
+ * - Salt (typically the public key)
+ * - Deployer address (0 for counterfactual deployment)
+ *
+ * DERIVATION PATH:
+ * Uses BIP-44 format: m/44'/9004'/accountIdx'/0/0
+ * - SLIP-044 coin type: 9004 (Starknet)
+ * - Community consensus: BIP-44 preferred over EIP-2645
+ * - See: https://community.starknet.io/t/account-keys-and-addresses-derivation-standard/1230
+ *
+ * REFERENCES:
+ * - Starknet Account Architecture: https://docs.starknet.io/architecture/accounts/
+ * - Account Abstraction Part I: https://medium.com/starknet-edu/account-abstraction-on-starknet-part-i-2ff84c6a3c30
+ * - Account Abstraction Part II: https://medium.com/starknet-edu/account-abstraction-on-starknet-part-ii-24d52874e0bd
+ * - Account Creation Guide: https://starknetjs.com/docs/guides/create_account/
+ * - OpenZeppelin Accounts: https://docs.openzeppelin.com/contracts-cairo/0.14.0/accounts
+ * - SLIP-044 Registry: https://github.com/satoshilabs/slips/blob/master/slip-0044.md
  */
 export class StarknetAdapter {
   protected readonly nodeAdapter: Isolation.Adapters.Stark;
 
   // OpenZeppelin account v0.15.0-rc.0 class hash (used by most wallets)
+  // See: https://docs.openzeppelin.com/contracts-cairo/0.14.0/accounts
   private readonly OZ_ACCOUNT_CLASS_HASH = "0x05b4b537eaa2399e3aa99c4e2e0208ebd6c71bc1467938cd52c798c601e43564";
 
   constructor(nodeAdapter: Isolation.Adapters.Stark) {
